acc2fa2aec8cde664eeed71cb3e79b7e7adb4cc8
jeromedeguzman <jerome@outsource2northstar.com>
2013-08-07 16:08:14 +0800

    add some changes

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -13,20 +13,24 @@ class Dashboard_Controller extends Base_Controller {
 	public function get_index() {
 		if(Account::is_supervisor(Auth::user()->id)) {
 			$callback_list = Callback::where('date', '>=', date('m/d/Y'))
+									->where('disposition_id' , '!=', '4')
 									->order_by('date', 'asc')
 									->get();
 
 			$ecallbacks_list =  Callback::where('date', '<', date('m/d/Y'))
+									->where('disposition_id' , '!=', '4')
 									->order_by('date', 'desc')
 									->get();
 		} else {
 			$account_id = Account::find(Auth::user()->id);
 			$callback_list = Callback::where('account_id' ,'=' , $account_id->id)
+									->where('disposition_id' , '!=', '4')
 									->where('date', '>=', date('m/d/Y'))
 									->order_by('date', 'asc')
 									->get();
 
 			$ecallbacks_list =  Callback::where('account_id' ,'=' , $account_id->id)
+									->where('disposition_id' , '!=', '4')
 									->where('date', '<', date('m/d/Y'))
 									->order_by('date', 'desc')
 									->get();
